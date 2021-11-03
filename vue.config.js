a46aefb957c46538be2280640520cfc7fbe3234e
lonelyinnovator <lonelyinnovator@users.noreply.github.com>
2021-11-03 19:51:52 +0800

    dist update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // 部署到gitee page/github page必备
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/online-ordering-system/#'
+        ? '/online-ordering-system'
         : '/'
 }
